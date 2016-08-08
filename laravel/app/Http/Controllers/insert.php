a985e823b9275e3d23e971246953a1c385023fe5
Arpan <arpanmittal123@gmail.com>
2016-08-08 13:38:57 +0530

    pushing formatted csv files

--- laravel/app/Http/Controllers/insert.php
+++ laravel/app/Http/Controllers/insert.php
@@ -95,9 +95,9 @@ class insert extends Controller
         $filename1="chaptertopicmap.csv";
         $csv1=$path.$filename1;
         $query = sprintf("LOAD DATA local INFILE '%s' INTO TABLE chaptertopicmap FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\n' IGNORE 1 LINES (`cl_su_st_ch_id`,`topic_id`,`order`,`hash`)", addslashes($csv1));
-        \DB::connection()->getpdo()->exec($query);
+       return \DB::connection()->getpdo()->exec($query);
         
-        return $this->chaptertopic($path);
+
     }
 
 }
