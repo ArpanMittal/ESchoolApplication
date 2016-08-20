b826bdbac9216f256915871866565aa77e358968
gglitch <punitchhajer@hotmail.com>
2016-08-20 13:06:19 +0530

    classes and streams details

--- laravel/app/Http/Controllers/DetailsController.php
+++ laravel/app/Http/Controllers/DetailsController.php
@@ -30,7 +30,7 @@ class DetailsController extends Controller
             return Response::json([
                 'success' => false,
                 'code' => 401,
-                'message' => 'Content not available'
+                'message' => 'Content is not available'
             ]);
         }
         $rows = array($row1, $row2, $row3);
@@ -38,6 +38,33 @@ class DetailsController extends Controller
             'success' => true,
             'code' => 200,
             'data' => $rows
+        ]);
+    }
+
+
+    public function getDetails(Request $request, $tag, $id){
+        switch ($tag){
+            case 'Classes':
+                $data = $this->getSubject($id);
+                break;
+            case 'Streams':
+                $data = $this->getStreams($id);
+                break;
+            case 'Exams':
+                $data = $this->getExamSubjects($id);
+        }
+
+        if (!$data){
+            return Response::json([
+                'success' => false,
+                'code' => 401,
+                'message' => 'Content is not available'
+            ]);
+        }
+        return Response::json([
+            'success' => true,
+            'code' => 200,
+            'data' => $data
         ]);
     }
 
@@ -68,8 +95,50 @@ class DetailsController extends Controller
         return DB::table('examtag')
             ->select('examtag.id as id',
                 'examtag.exam_name as name',
-                DB::raw('count(DISTINCT examtag.id) as count'))
+                DB::raw('"empty" as count'))
             ->where('exam_name', 'NOT LIKE', 'CBSE')
             ->get();
+    }
+
+    private function getChapter($id){
+        return DB::table('streamchaptermap')
+            ->select('streamchaptermap.cl_su_st_ch_id as id',
+                'chapter.chapter_name as name',
+                DB::raw('count(DISTINCT chaptertopicmap.hash) as count'))
+            ->join('chapter','streamchaptermap.chapter_id','=','chapter.id')
+            ->join('chaptertopicmap','streamchaptermap.cl_su_st_ch_id','=','chaptertopicmap.cl_su_st_ch_id')
+            ->where('streamchaptermap.cl_su_st_id',$id)
+            ->groupBy('streamchaptermap.chapter_id')
+            ->get();
+    }
+    private function getSubject($id){
+        return DB::table('classsubjectmap')
+            ->select('classsubjectmap.cl_su_id as id',
+                'subject.subject_name as name',
+                DB::raw('count(DISTINCT streamchaptermap.cl_su_st_ch_id) as count'))
+            ->join('subject','classsubjectmap.subject_id','=','subject.id')
+            ->join('subjectstreammap','classsubjectmap.cl_su_id','=','subjectstreammap.cl_su_id')
+            ->join('streamchaptermap','subjectstreammap.cl_su_st_id','=','streamchaptermap.cl_su_st_id')
+            ->where('classsubjectmap.class_id',$id)
+            ->groupBy('classsubjectmap.subject_id')
+            ->get();
+    }
+
+    private function getStreams($id)
+    {
+        return DB::table('subjectstreammap')
+            ->select('subjectstreammap.cl_su_st_id as id',
+                'stream.stream_name as name',
+                DB::raw('count(DISTINCT streamchaptermap.cl_su_st_ch_id) as count'))
+            ->join('stream','subjectstreammap.stream_id','=','stream.id')
+            ->join('streamchaptermap','subjectstreammap.cl_su_st_id','=','streamchaptermap.cl_su_st_id')
+            ->where('subjectstreammap.cl_su_id','LIKE',"%".$id)
+            ->groupBy('subjectstreammap.stream_id')
+            ->get();
+    }
+
+    private function getExamSubjects($id)
+    {
+        return;
     }
 }
