2387343dcd1670160d535716f97324bffd1c1d1d
Arpan <arpanmittal123@gmail.com>
2016-08-23 12:33:06 +0530

    dash board complete view

--- E-School/app/src/main/java/com/organization/sjhg/e_school/Helpers/RecyclerAdapter.java
+++ E-School/app/src/main/java/com/organization/sjhg/e_school/Helpers/RecyclerAdapter.java
@@ -5,25 +5,28 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.organization.sjhg.e_school.ListStructure.DashBoardList;
 import com.organization.sjhg.e_school.ListStructure.InternalList;
 import com.organization.sjhg.e_school.R;
 
-import java.util.ArrayList;
+import java.security.PrivateKey;
 import java.util.List;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<View_Holder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<View_Holder>{
 
     private List<InternalList> list;
     private Context context;
+    private String title;
+    private Activity activity;
+    private RecyclerView recyclerView;
 
-    public RecyclerAdapter(Context context, List<InternalList> list) {
+
+    public RecyclerAdapter(Context context, List<InternalList> list,String title,Activity activity,RecyclerView recyclerView) {
         this.list = list;
         this.context = context;
+        this.title=title;
+        this.activity=activity;
+        this.recyclerView=recyclerView;
     }
 
     @Override
@@ -35,6 +38,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<View_Holder> {
 
         View_Holder view_Holder = new View_Holder(view);
 
+
         return view_Holder;
     }
 
@@ -43,6 +47,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<View_Holder> {
         viewHolder.name.setText(list.get(position).name);
         viewHolder.count.setText(list.get(position).count);
         viewHolder.imageView.setImageResource(R.drawable.notechathead);
+        viewHolder.title=this.title;
+        viewHolder.id=list.get(position).id;
+        viewHolder.recyclerView=recyclerView;
+        viewHolder.activity=activity;
+       // viewHolder.setClickListener(new View.OnClickListener() {
+
 
     }
 
@@ -54,8 +64,6 @@ public class RecyclerAdapter extends RecyclerView.Adapter<View_Holder> {
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
     }
-
-
 
 
 
--- E-School/app/src/main/java/com/organization/sjhg/e_school/Helpers/Recycler_Child_Adapter.java
+++ E-School/app/src/main/java/com/organization/sjhg/e_school/Helpers/Recycler_Child_Adapter.java
@@ -3,9 +3,12 @@ package com.organization.sjhg.e_school.Helpers;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.organization.sjhg.e_school.ListStructure.InternalList;
+import com.organization.sjhg.e_school.R;
 
 import java.util.List;
 
@@ -27,18 +30,30 @@ public class Recycler_Child_Adapter extends RecyclerView.Adapter<Child_View_Hold
     }
 
     @Override
-    public Child_View_Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public Child_View_Holder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        //inflate your layout and pass it to view holder
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.activitycardview, viewGroup, false);
+        Child_View_Holder child_view_holder=new Child_View_Holder(view);
+
+
+        return child_view_holder;
     }
 
 
     @Override
     public void onBindViewHolder(Child_View_Holder holder, int position) {
 
+        holder.name.setText(list.get(position).name);
+        holder.count.setText(list.get(position).count);
+        holder.imageView.setImageResource(R.drawable.notechathead);
+        holder.title=this.title;
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return list.size();
     }
 }
