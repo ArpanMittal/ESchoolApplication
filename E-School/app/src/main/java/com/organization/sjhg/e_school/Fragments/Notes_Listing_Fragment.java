3d49528d9338a6f42c7460d46f03359bc40bcaa7
Arpan <arpanmittal123@gmail.com>
2016-08-25 17:38:52 +0530

    notes activity

---
+++ E-School/app/src/main/java/com/organization/sjhg/e_school/Fragments/Notes_Listing_Fragment.java
@@ -0,0 +1,36 @@
+package com.organization.sjhg.e_school.Fragments;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.organization.sjhg.e_school.MainParentActivity;
+import com.organization.sjhg.e_school.R;
+import com.organization.sjhg.e_school.TakeNotes.AddSmallNotesActivity;
+import com.organization.sjhg.e_school.TakeNotes.NoteListingActivity;
+
+/**
+ * Created by arpan on 8/25/2016.
+ */
+public class Notes_Listing_Fragment extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes_listing_activity);
+        Fragment fragment=new NoteListingActivity();
+        getFragmentManager().beginTransaction().replace(R.id.notesfragment1, new NoteListingActivity()).commit();
+        View addnote= findViewById(R.id.addnotebutton);
+        addnote.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), AddSmallNotesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
