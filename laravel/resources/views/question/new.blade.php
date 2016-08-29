cdc7383a45074b680c106ec168131449cd400804
gglitch <punitchhajer@hotmail.com>
2016-08-29 13:54:32 +0530

    solving edit question errors...

--- laravel/resources/views/question/new.blade.php
+++ laravel/resources/views/question/new.blade.php
@@ -107,7 +107,7 @@
                                                             <input class="" type="button" name="question_diagram_button" id="question_diagram_button" value="Change Image" />
                                                             <img src="{!! asset($question->image_path) !!}" />
                                                         @else
-                                                            <input class="" type="file" name="question_diagram" id="question_diagram"  />
+                                                            <input class="" type="file" name="question_diagram" id="question_diagram" accept="image/*" />
                                                         @endif
                                                     </div>
                                                 </div>
@@ -162,7 +162,7 @@
                                                         <input class="" type="button" name="solution_button" id="solution_button" value="Change Image" />
                                                         <img src="{!! asset($question->solution_path) !!}" />
                                                     @else
-                                                        <input class="" type="file" name="solution" id="solution"  />
+                                                        <input class="" type="file" name="solution" id="solution" accept="image/*" />
                                                     @endif
                                                 </div>
                                             </div>
@@ -330,6 +330,7 @@
                 data: formData,
                 async: false,
                 success: function (data) {
+                    alert(JSON.stringify(data));
                     data = JSON.parse(data);
                     if(data.success=='false'){
                         alert(JSON.stringify(data.error));
