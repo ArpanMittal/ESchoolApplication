5b01e990e471bcc64140721369a0d53014f88ea5
Shreyas Tripathy <shreyas@hitechgurukul.com>
2016-09-02 12:19:37 +0530

    navigation fix

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -1,8 +1,11 @@
 @section('navigation')
-    @if (isset($user) || $user->role_id==1 || $user->role_id==2 || $user->role_id==3)
-        <li><a href="{{ url('/question/list') }}">Question</a></li>
-        <li><a href="{{ url('/content/list/class') }}">Content</a></li>
-    @elseif(isset($user) || $user->role_id==1)
-        <li><a href="{{ url('/package/list') }}">Package</a></li>
+    @if(isset($user))
+        @if ($user->role_id==1 || $user->role_id==2 || $user->role_id==3)
+            <li><a href="{{ url('/question/list') }}">Question</a></li>
+            <li><a href="{{ url('/content/list/class') }}">Content</a></li>
+        @endif
+        @if($user->role_id==1)
+            <li><a href="{{ url('/package/list') }}">Package</a></li>
+        @endif
     @endif
 @endsection
