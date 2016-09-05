f6b7ffd85e758541cff9ed954007d6d97340feee
Arpan <arpanmittal123@gmail.com>
2016-09-05 11:34:34 +0530

    package optimization

--- laravel/database/migrations/2016_09_05_045722_packageoptimization.php
+++ laravel/database/migrations/2016_09_05_045722_packageoptimization.php
@@ -13,39 +13,42 @@ class Packageoptimization extends Migration
     public function up()
     {
         //
+        Schema::table('package', function ($table) {
+            $table->integer('duration');
+        });
 
         Schema::table('subscription', function ($table) {
             $table->dropColumn('item_id');
         });
         
         
-        Schema::create('sub_subject_map',function(Blueprint $table)
+        Schema::create('pack_subject_map',function(Blueprint $table)
         {
             $table->engine='InnoDB';
             $table->increments('id');
-            $table->integer('sub_id')->unsigned();
+            $table->integer('pack_id')->unsigned();
             $table->string('subject_id');
-            $table->Foreign('sub_id')->references('id')->on('subscription')->onDelete('cascade');
+            $table->Foreign('pack_id')->references('id')->on('package')->onDelete('cascade');
             $table->Foreign('subject_id')->references('id')->on('subject')->onDelete('cascade');;
         });
 
-        Schema::create('sub_subject_chapter_map',function(Blueprint $table)
+        Schema::create('pack_subject_chapter_map',function(Blueprint $table)
         {
             $table->engine='InnoDB';
             $table->increments('id');
-            $table->integer('sub_subject_id')->unsigned();
+            $table->integer('pack_subject_id')->unsigned();
             $table->string('chapter_id');
-            $table->Foreign('sub_subject_id')->references('id')->on('sub_subject_map')->onDelete('cascade');
+            $table->Foreign('pack_subject_id')->references('id')->on('pack_subject_map')->onDelete('cascade');
             $table->Foreign('chapter_id')->references('id')->on('chapter')->onDelete('cascade');;
         });
 
-        Schema::create('sub_subject_chapter_topic_map',function(Blueprint $table)
+        Schema::create('pack_subject_chapter_topic_map',function(Blueprint $table)
         {
             $table->engine='InnoDB';
             $table->increments('id');
-            $table->integer('sub_subject_chapter_id')->unsigned();
+            $table->integer('pack_subject_chapter_id')->unsigned();
             $table->string('topic_id');
-            $table->Foreign('sub_subject_chapter_id')->references('id')->on('sub_subject_chapter_map')->onDelete('cascade');
+            $table->Foreign('pack_subject_chapter_id')->references('id')->on('pack_subject_chapter_map')->onDelete('cascade');
             $table->Foreign('topic_id')->references('id')->on('topic')->onDelete('cascade');;
         });
         Schema::table('subscription', function ($table) {
@@ -53,6 +56,8 @@ class Packageoptimization extends Migration
             $table->dropColumn('type_id');
         });
         Schema::dropIfExists('subscriptiontype');
+        Schema::dropIfExists('packagesubmap');
+        Schema::dropIfExists('subscription');
 
         
     }
@@ -69,6 +74,30 @@ class Packageoptimization extends Migration
         Schema::drop('subscription_subject_chapter_map');
         Schema::drop('subscription_subject_map');
 
+        Schema::create('subscription', function (Blueprint $table) {
+            $table->engine='InnoDB';
+            $table->increments('id');
+            $table->datetime('created_at');
+
+
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            // $table->integer('By')->unsigned();
+            $table->integer('type_id')->unsigned();
+            $table->integer('duration')->default(12);
+            $table->integer('item_id');
+            $table->foreign('type_id')->references('id')->on('subscriptiontype')->onDelete('cascade');
+        });
+
+        Schema::create('packagesubmap',function(Blueprint$table){
+            $table->engine='InnoDB';
+            $table->increments('id');
+            $table->integer('pack_id')->unsigned();
+            $table->integer('sub_id')->unsigned();
+            $table->foreign('pack_id')->references('id')->on('package')->onDelete('cascade');
+            $table->foreign('sub_id')->references('id')->on('subscription')->onDelete('cascade');
+
+        });
+
         Schema::create('subscriptiontype', function (Blueprint $table) {
             $table->engine='InnoDB';
             $table->increments('id');
@@ -83,6 +112,10 @@ class Packageoptimization extends Migration
             $table->string('item_id');
             $table->integer('type_id');
             $table->foreign('type_id')->references('id')->on('subscriptiontype')->onDelete('cascade');
+        });
+
+        Schema::table('package', function ($table) {
+            $table->dropColumn('duration');
         });
     }
 }
