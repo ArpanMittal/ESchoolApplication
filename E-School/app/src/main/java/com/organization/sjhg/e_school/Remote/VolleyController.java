cfb0910f4bd1ff5797b69c6dddc94fc1c816e2ba
gglitch <punitchhajer@hotmail.com>
2016-09-23 14:01:33 +0530

    change app icon...

--- E-School/app/src/main/java/com/organization/sjhg/e_school/Remote/VolleyController.java
+++ E-School/app/src/main/java/com/organization/sjhg/e_school/Remote/VolleyController.java
@@ -26,10 +26,10 @@ public class VolleyController extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FontsOverride.setDefaultFont(this, "DEFAULT", "TitilliumWeb-LightItalic.ttf");
+//        FontsOverride.setDefaultFont(this, "DEFAULT", "TitilliumWeb-LightItalic.ttf");
         FontsOverride.setDefaultFont(this, "MONOSPACE", "TitilliumWeb-LightItalic.ttf");
-        FontsOverride.setDefaultFont(this, "SERIF", "TitilliumWeb-LightItalic.ttf");
-        FontsOverride.setDefaultFont(this, "SANS_SERIF", "TitilliumWeb-LightItalic.ttf");
+//        FontsOverride.setDefaultFont(this, "SERIF", "TitilliumWeb-LightItalic.ttf");
+//        FontsOverride.setDefaultFont(this, "SANS_SERIF", "TitilliumWeb-LightItalic.ttf");
         mInstance = this;
     }
 
