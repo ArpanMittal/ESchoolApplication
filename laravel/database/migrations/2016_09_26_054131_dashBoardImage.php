d68fa54bdc4bd3b9b299dba05d743756e14dbf3e
Arpan <arpanmittal123@gmail.com>
2016-09-26 11:22:34 +0530

    dashboard_image table

---
+++ laravel/database/migrations/2016_09_26_054131_dashBoardImage.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DashBoardImage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('dashboard_image',function(Blueprint$table){
+            $table->engine='InnoDB';
+            $table->increments('id');
+            $table->string('image_path');
+            $table->datetime('text');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::statement('DROP TABLE dashboard_image');
+    }
+}
